957d4154299bf24276ddc64b02192ebad5d4f361
Wakil Sarfaraz <wakilsarfaraz@Wakils-MacBook-Pro.local>
2015-11-24 18:39:49 +0000

    for test on my office machine

--- step-2.cc
+++ step-2.cc
@@ -23,50 +23,16 @@
 #include <deal.II/grid/tria_accessor.h>
 #include <deal.II/grid/tria_iterator.h>
 #include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/tria_boundary_lib.h>
+#include <deal.II/grid/manifold_lib.h>
 #include <deal.II/grid/grid_out.h>
-
 #include <deal.II/dofs/dof_handler.h>
-
 #include <deal.II/fe/fe_q.h>
 #include <deal.II/dofs/dof_tools.h>
 #include <deal.II/lac/sparse_matrix.h>
-#include <deal.II/lac/compressed_sparsity_pattern.h>
-
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
 #include <deal.II/dofs/dof_renumbering.h>
-
 #include <fstream>
-
-//Uptodate
-
 using namespace dealii;
-
-void make_grid (Triangulation<3> &triangulation)
-{
-    GridGenerator::hyper_cube(triangulation);
-    static const types::manifold_id flat_manifold_id = static_cast<types::manifold_id>(-1);
-    triangulation.refine_global(3);
-    
-    std::ofstream out("cube.vtk");
-    GridOut grid_out;
-    grid_out.write_vtk(triangulation,out);
-    std::cout<<"See mesh in cube.vtk"<<std::endl;
-    
-}
-
-
-/*void make_grid (Triangulation<2> &triangulation)
-{
-    GridGenerator::hyper_cube(triangulation);
-    static const types::manifold_id flat_manifold_id = static_cast<types::manifold_id>(-1);
-    triangulation.refine_global(6);
-   
-    std::ofstream out("square.eps");
-    GridOut grid_out;
-    grid_out.write_eps(triangulation,out);
-    std::cout<<"See mesh in square.eps"<<std::endl;
-}*/
-
 /*void make_grid (Triangulation<2> &triangulation)
 {
     const Point<2> center (1,0);
@@ -75,16 +41,14 @@ void make_grid (Triangulation<3> &triangulation)
     GridGenerator::hyper_shell (triangulation,
                                 center, inner_radius, outer_radius,
                                 10);
-    
-    static const HyperShellBoundary<2> boundary_description(center);
-    triangulation.set_boundary (0, boundary_description);
-    
-    for (unsigned int step=0; step<6; ++step)
+    static const SphericalManifold<2> manifold_description(center);
+    triangulation.set_all_manifold_ids(0);
+    triangulation.set_manifold (0, manifold_description);
+    for (unsigned int step=0; step<5; ++step)
     {
         Triangulation<2>::active_cell_iterator
         cell = triangulation.begin_active(),
         endc = triangulation.end();
-        
         for (; cell!=endc; ++cell)
             for (unsigned int v=0;
                  v < GeometryInfo<2>::vertices_per_cell;
@@ -92,117 +56,94 @@ void make_grid (Triangulation<3> &triangulation)
             {
                 const double distance_from_center
                 = center.distance (cell->vertex(v));
-                
                 if (std::fabs(distance_from_center - inner_radius) < 1e-10 ||
-                    std::fabs(distance_from_center - outer_radius) < 1e-10 ||
-                    std::fabs(distance_from_center - (inner_radius+(outer_radius-inner_radius)/2)) < 1e-10)
+                    std::fabs(distance_from_center - outer_radius) < 1e-10 )
                 {
                     cell->set_refine_flag ();
                     break;
                 }
             }
-        
         triangulation.execute_coarsening_and_refinement ();
     }
-    std::ofstream out("domain_grid.eps");
+    std::ofstream out ("shell.eps");
     GridOut grid_out;
     grid_out.write_eps (triangulation, out);
-    std::cout<<"Open domain_grid.eps to see the mesh"<<std::endl;
-    triangulation.set_boundary(0);
+    std::cout << "Shell mesh in 2d is written to shell.eps" << std::endl;
 }*/
 
-/*void make_grid (Triangulation<3> &triangulation)
+void make_grid (Triangulation<2> &triangulation)
 {
-    const Point<3> center (1,0,0);
+    const Point<2> center (1,0);
     const double inner_radius = 0.5,
     outer_radius = 1.0;
     GridGenerator::hyper_shell (triangulation,
                                 center, inner_radius, outer_radius,
-                                96,false);
-    static const HyperShellBoundary<3> boundary_description(center);
-    triangulation.set_boundary (0, boundary_description);
-    
-    for (unsigned int step=0; step<2; ++step)
+                                10);
+    triangulation.set_all_manifold_ids(0);
+    const static SphericalManifold<2> manifold_description(center);
+    triangulation.set_manifold (0, manifold_description);
+    for (unsigned int step=0; step<5; ++step)
     {
-        Triangulation<3>::active_cell_iterator cell = triangulation.begin_active(), endc = triangulation.end();
-        
+        Triangulation<2>::active_cell_iterator
+        cell = triangulation.begin_active(),
+        endc = triangulation.end();
         for (; cell!=endc; ++cell)
+        {
             for (unsigned int v=0;
-                 v < GeometryInfo<3>::vertices_per_cell;
+                 v < GeometryInfo<2>::vertices_per_cell;
                  ++v)
             {
                 const double distance_from_center
                 = center.distance (cell->vertex(v));
-                
-                if (std::fabs(distance_from_center - inner_radius) > 1e-10 ||
-                                                                           std::fabs(distance_from_center - outer_radius) < 1e-10 ||
-                                                                           std::fabs(distance_from_center - (inner_radius+(inner_radius-outer_radius)/2))<1e-10)
-              {
+                if (std::fabs(distance_from_center - inner_radius) < 1e-10 ||
+                    std::fabs(distance_from_center - outer_radius) < 1e-10 ||
+                    std::fabs(distance_from_center-(inner_radius+(outer_radius-inner_radius)/2))<1e-10/* ||
+                                                                                                       std::fabs(distance_from_center-(inner_radius+(outer_radius-inner_radius)/0.5))<1e-10*/)
+                {
                     cell->set_refine_flag ();
                     break;
                 }
             }
-        
+        }
         triangulation.execute_coarsening_and_refinement ();
     }
-    
-    
-    std::ofstream out ("shell-3d.vtk");
+    std::ofstream out ("grid-2.eps");
     GridOut grid_out;
-    grid_out.write_vtk (triangulation, out);
-    
-    std::cout << "Shell mesh in 3d is written to shell-3d.vtk" << std::endl;
-    
-    triangulation.set_boundary (0);
-}*/
-
-
-
-void distribute_dofs (DoFHandler<3> &dof_handler)
-{
-  static const FE_Q<3> finite_element(1);
-  dof_handler.distribute_dofs (finite_element);
-
-  CompressedSparsityPattern compressed_sparsity_pattern(dof_handler.n_dofs(),
-                                                        dof_handler.n_dofs());
-
-  DoFTools::make_sparsity_pattern (dof_handler, compressed_sparsity_pattern);
-
-  SparsityPattern sparsity_pattern;
-  sparsity_pattern.copy_from (compressed_sparsity_pattern);
-
-  std::ofstream out ("sparsity_pattern.1");
-  sparsity_pattern.print_gnuplot (out);
+    grid_out.write_eps (triangulation, out);
+    std::cout << "Grid written to grid-2.eps" << std::endl;
+    triangulation.set_manifold (0);
 }
 
 
 
-void renumber_dofs (DoFHandler<3> &dof_handler)
+void distribute_dofs (DoFHandler<2> &dof_handler)
 {
-  DoFRenumbering::Cuthill_McKee/*hierarchical*/ (dof_handler);
-
-  CompressedSparsityPattern compressed_sparsity_pattern(dof_handler.n_dofs(),
-                                                        dof_handler.n_dofs());
-  DoFTools::make_sparsity_pattern (dof_handler, compressed_sparsity_pattern);
-
-  SparsityPattern sparsity_pattern;
-  sparsity_pattern.copy_from (compressed_sparsity_pattern);
-
-  std::ofstream out ("sparsity_pattern.2");
-  sparsity_pattern.print_gnuplot (out);
+    static const FE_Q<2> finite_element(1);
+    dof_handler.distribute_dofs (finite_element);
+    DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs(),
+                                                    dof_handler.n_dofs());
+    DoFTools::make_sparsity_pattern (dof_handler, dynamic_sparsity_pattern);
+    SparsityPattern sparsity_pattern;
+    sparsity_pattern.copy_from (dynamic_sparsity_pattern);
+    std::ofstream out ("sparsity_pattern.1");
+    sparsity_pattern.print_gnuplot (out);
 }
-
-
-
-
-
+void renumber_dofs (DoFHandler<2> &dof_handler)
+{
+    DoFRenumbering::Cuthill_McKee (dof_handler);
+    DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs(),
+                                                    dof_handler.n_dofs());
+    DoFTools::make_sparsity_pattern (dof_handler, dynamic_sparsity_pattern);
+    SparsityPattern sparsity_pattern;
+    sparsity_pattern.copy_from (dynamic_sparsity_pattern);
+    std::ofstream out ("sparsity_pattern.2");
+    sparsity_pattern.print_gnuplot (out);
+}
 int main ()
 {
-  Triangulation<3> triangulation;
-  make_grid (triangulation);
-
-  DoFHandler<3> dof_handler (triangulation);
-
-  distribute_dofs (dof_handler);
-  renumber_dofs (dof_handler);
+    Triangulation<2> triangulation;
+    make_grid (triangulation);
+    DoFHandler<2> dof_handler (triangulation);
+    distribute_dofs (dof_handler);
+    renumber_dofs (dof_handler);
 }
